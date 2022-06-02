86cbe308fff6dedf72fa46b06fb3f2381192aec6
Egor Gorbunov <gorbunov.ed@gmail.com>
2022-06-02 12:44:20 +0300

    done

---
+++ pages/index.js
@@ -0,0 +1,57 @@
+let heart = document.querySelector('.elements__heart');
+heart.addEventListener('click', function(e) {
+    heart.classList.add('elements__heartBlack');
+});
+
+const button = document.querySelector('.profile__button');
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close-button');
+button.addEventListener('click', function() {
+    popup.classList.remove('popup_hidden');
+});
+popupCloseButton.addEventListener('click', function() {
+    popup.classList.add('popup_hidden');
+});
+let formElement = document.querySelector('.popup__content');
+let nameInput = document.querySelector('.popup__input');
+let jobInput = document.querySelector('.popup__inputprof');
+console.log(nameInput.value);
+console.log(jobInput.value);
+const buttonSave = document.querySelector('.popup__save-button');
+let profileName = document.querySelector('.profile__fullname');
+let job = document.querySelector('.profile__profession');
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+}
+let NewJobInput = jobInput.value;    
+let NewName = nameInput.value;
+buttonSave.addEventListener('click', function() {
+    profileName.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    popup.classList.add('popup_hidden');
+});
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
