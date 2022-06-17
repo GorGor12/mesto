c38e0b957310ce516f72dd46ff8d3ade82547480
Egor Gorbunov <gorbunov.ed@gmail.com>
2022-06-17 22:51:45 +0300

    fixing easy closing popup

--- script/index.js
+++ script/index.js
@@ -2,6 +2,7 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const popupCloseButtonNewPlace = document.querySelector('.popup__close-button_NewPlace');
+const popupCloseButtonImage = document.querySelector('.popup__close-button_image');
 const formElement = document.querySelector('.popup__form');
 const formElementNewPlace = document.querySelector('.popup__form_newPlace');
 
@@ -10,7 +11,7 @@ const jobInput = document.querySelector('.popup__input_value_job');
 const profileName = document.querySelector('.profile__full-name');
 const job = document.querySelector('.profile__profession');
 const elementsTitle = document.querySelector('.elements__title');
-const elementsImage = document.querySelector('.elements__image');
+// const elementsImage = document.querySelector('.elements__image');
 
 const profileAddButton = document.querySelector('.profile__add-button');
 const popupNewPlace = document.querySelector('.popupNewPlace');
@@ -20,7 +21,16 @@ const section = document.querySelector('.elements'); //куда добавляе
 const itemTemplate = document.querySelector('.item_template').content; //что добавляем
 
 const itemTemplateCard = itemTemplate.querySelector('.elements__item').cloneNode(true);
-const buttonHeart = itemTemplateCard.querySelector('.elements__heart');
+
+
+
+// const basket = itemTemplate.querySelector('.elements__basket').cloneNode(true);
+const newBasket = itemTemplateCard.querySelector('.elements__basket');
+
+
+
+
+
 
 const initialCards = [
   {
@@ -48,14 +58,33 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
 initialCards.forEach(function(element) {
   const itemTemplateCard = itemTemplate.querySelector('.elements__item').cloneNode(true);
+  const basket = itemTemplateCard.querySelector('.elements__basket');
+  const buttonHeart = itemTemplateCard.querySelector('.elements__heart');
+  const elementsImage = itemTemplateCard.querySelector('.elements__image');
+  const popupImage = document.querySelector('.popupImage');
+  const popupCloseButton = document.querySelector('.popup__close-button');
   itemTemplateCard.querySelector('.elements__image').src = element.link;
   itemTemplateCard.querySelector('.elements__title').textContent = element.name;
+  buttonHeart.addEventListener('click', function() {
+    buttonHeart.classList.toggle('elements__heart_active');
+  });
+  basket.addEventListener('click', function() {
+    itemTemplateCard.remove();
+  });
+  elementsImage.addEventListener('click', function() {
+    popupImage.classList.add('popupImage_open');
+    document.querySelector('.popupImage__image').src = element.link;
+    document.querySelector('.popupImage__text').textContent = element.name;
+    console.log(1);
+  });
+  popupCloseButtonImage.addEventListener('click', function() {
+    popupImage.classList.remove('popupImage_open');
+  });
   section.append(itemTemplateCard);
 });
-
-
 
 
 function formSubmitHandler(evt) {
@@ -67,10 +96,30 @@ function formSubmitHandler(evt) {
 
 function formSubmitHandlerNewPlace(evt) {
   const itemTemplateCardNew = itemTemplate.querySelector('.elements__item').cloneNode(true);
+  const elementsImage = itemTemplateCardNew.querySelector('.elements__image');
+  const popupImage = document.querySelector('.popupImage');
+  const popupCloseButton = document.querySelector('.popup__close-button');
   evt.preventDefault();
   section.prepend(itemTemplateCardNew);
   itemTemplateCardNew.querySelector('.elements__title').textContent = placeInput.value;
   itemTemplateCardNew.querySelector('.elements__image').src = srcInput.value;
+  const newBasket = itemTemplateCardNew.querySelector('.elements__basket');
+  newBasket.addEventListener('click', function(evt) {
+    itemTemplateCardNew.remove();
+  });
+  const buttonHeart = itemTemplateCardNew.querySelector('.elements__heart');
+  buttonHeart.addEventListener('click', function() {
+    buttonHeart.classList.toggle('elements__heart_active');
+  });
+  elementsImage.addEventListener('click', function() {
+    console.log(1);
+    popupImage.classList.add('popupImage_open');
+    document.querySelector('.popupImage__image').src = srcInput.value;
+    document.querySelector('.popupImage__text').textContent = placeInput.value;
+  });
+  popupCloseButtonImage.addEventListener('click', function() {
+    popupImage.classList.remove('popupImage_open');
+  });
   closeNewPlacePopup();
 }
 
@@ -102,9 +151,11 @@ function openPlacePopup() {
   popup.classList.add('popup_open');
 }
 
-function changeHeart() {
-  buttonHeart.classList.add('elements__heart_active');
-}
+
+// popupCloseButtonImage.addEventListener('click', function() {
+  // console.log(1);
+  // popupImage.classList.remove('popupImage_open');
+// });
 
 profileEditButton.addEventListener('click', openPopup);
 
@@ -116,8 +167,15 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 formElementNewPlace.addEventListener('submit', formSubmitHandlerNewPlace);
 
+// newBasket.addEventListener('click', function(evt) {
+//   const eventTarget = evt.target;
+//   eventTarget.remove;
+// });
 
-buttonHeart.addEventListener('click', changeHeart);
+
+
+
+
 
 
 
